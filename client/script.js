ed1edd635cda689458c3bf5d0a97d87ce96f20d5
viru <38450997+HRithIkGowDA@users.noreply.github.com>
2023-02-10 13:12:37 +0530

    Update script.js

--- client/script.js
+++ client/script.js
@@ -86,7 +86,7 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
 
-    const response = await fetch('https://codex-79o8.onrender.com/', {
+    const response = await fetch('https://codex-79o8.onrender.com/', {  //http://localhost:5000 for running in local machine
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
